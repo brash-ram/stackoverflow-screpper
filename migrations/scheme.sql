0e6907808293ad6b304a8d86949f2fc767f49568
Rudi <rudi.will.do.it@gmail.com>
2023-04-11 23:20:02 +0300

    add integration test

--- migrations/scheme.sql
+++ migrations/scheme.sql
@@ -1,15 +1,22 @@
-CREATE TABLE "links"(
-    "id" BIGINT NOT NULL,
-    "url" VARCHAR(255) NOT NULL,
-    "chat" BIGINT NOT NULL
-);
+CREATE SEQUENCE IF NOT EXISTS links_id_seq start 1;
+
+CREATE TABLE IF NOT EXISTS links(
+    id BIGINT NOT NULL DEFAULT nextval('links_id_seq') PRIMARY KEY,
+    url VARCHAR(255) NOT NULL,
+    chat BIGINT NOT NULL
+    );
+
+ALTER SEQUENCE links_id_seq
+    OWNED BY links.id;
+
+
+CREATE SEQUENCE IF NOT EXISTS chats_id_seq start 1;
+CREATE TABLE IF NOT EXISTS chats(
+    id BIGINT NOT NULL DEFAULT nextval('chats_id_seq') PRIMARY KEY,
+    chat_id BIGINT NOT NULL
+    );
+
 ALTER TABLE
-    "links" ADD PRIMARY KEY("id");
-CREATE TABLE "chats"(
-    "id" BIGINT NOT NULL,
-    "chat_id" BIGINT NOT NULL
-);
-ALTER TABLE
-    "chats" ADD PRIMARY KEY("id");
-ALTER TABLE
-    "links" ADD CONSTRAINT "links_chat_foreign" FOREIGN KEY("chat") REFERENCES "chats"("id");
+    links ADD CONSTRAINT links_chat_foreign FOREIGN KEY(chat) REFERENCES chats(id);
+ALTER SEQUENCE chats_id_seq
+    OWNED BY chats.id;
---
+++ scrapper/src/test/java/ru/tinkoff/edu/scrapper/environment/IntegrationEnvironment.java
@@ -0,0 +1,76 @@
+package ru.tinkoff.edu.scrapper.environment;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@ContextConfiguration(classes = IntegrationEnvironment.IntegrationEnvironmentConfiguration.class)
+public abstract class IntegrationEnvironment {
+
+    @Configuration
+    public static class IntegrationEnvironmentConfiguration {
+
+        @Bean
+        public DataSource testDataSource() {
+            return DataSourceBuilder.create()
+                    .url(POSTGRESQL_CONTAINER.getJdbcUrl())
+                    .username(POSTGRESQL_CONTAINER.getUsername())
+                    .password(POSTGRESQL_CONTAINER.getPassword())
+                    .build();
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+    }
+
+    static protected final PostgreSQLContainer POSTGRESQL_CONTAINER;
+
+    static {
+        String username = "postgres";
+        String password = "qwerty";
+        String dbName = "postgres";
+        Integer dbPort = 5432;
+        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+                .withDatabaseName(dbName)
+                .withUsername(username)
+                .withPassword(password)
+                .withExposedPorts(dbPort);
+        POSTGRESQL_CONTAINER.start();
+    }
+
+    public static void runMigration() {
+        try {
+            String url = POSTGRESQL_CONTAINER.getJdbcUrl();
+            String username = POSTGRESQL_CONTAINER.getUsername();
+            String password = POSTGRESQL_CONTAINER.getPassword();
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Path changeLogPath = new File(".").toPath().toAbsolutePath().getParent().getParent()
+                    .resolve("migrations");
+            Liquibase liquibase = new liquibase.Liquibase("master.xml",
+                    new DirectoryResourceAccessor(changeLogPath), database);
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
