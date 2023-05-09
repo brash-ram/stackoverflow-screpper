6c451c87daec634f83d8488b6db7d073b4ae4cfc
Rudi <rudi.will.do.it@gmail.com>
2023-05-09 23:44:27 +0300

    some fix

--- link-parser/src/main/java/ru/tinkoff/edu/linkParser/LinkParserApplication.java
+++ link-parser/src/main/java/ru/tinkoff/edu/linkParser/LinkParserApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class LinkParserApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(LinkParserApplication.class, args);
     }
